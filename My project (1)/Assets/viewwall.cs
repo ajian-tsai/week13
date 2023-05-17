0ef1ba9a3c7ef2f372233f869504c509b7365099
ajian-tsai <chiufocal520@gmail.com>
2023-05-17 11:48:15 +0800

    add_牆面移動程式_地板跟天空

--- My project (1)/Assets/viewwall.cs
+++ My project (1)/Assets/viewwall.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 public class viewwall : MonoBehaviour
 {
     public GameObject[] wall;
-    private float timer = 0;
-    
+   // private float timer = 0;
+    public float speed = 1.0f;
+    private bool hasWall = false;
+    private int nowWall;
+
     void Start()
     {
         for (int i = 0; i < wall.Length; i++)
@@ -19,16 +22,28 @@ public class viewwall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("a")) {
-            wall[1].gameObject.SetActive(true);
-            wall[1].transform.position = new Vector3(0, 5, -20);
-            wall[1].transform.Translate( Vector3.forward * Time.deltaTime*2 );
-            timer += Time.deltaTime;
+        if (Input.GetKey("a") && !hasWall) {
+            nowWall = Random.Range(0, 10); // 隨機取牆
+            wall[nowWall].gameObject.SetActive(true); // 顯示牆
+            wall[nowWall].transform.position = new Vector3(0, 5, -20);  // 牆的初始位置
+            hasWall = true;
+           /* timer += Time.deltaTime;
             Debug.Log(timer);
             if (timer > 5)
             {
                 wall[1].gameObject.SetActive(false);
                 timer = 0;
+            }*/
+        }
+        if (hasWall)
+        {
+            wall[nowWall].transform.Translate(Vector3.up * speed * Time.deltaTime);
+            if (Input.GetKey("z"))
+            {
+                wall[nowWall].gameObject.SetActive(false);
+
+                // wall[noWall].
+                hasWall = false;
             }
         }
         
